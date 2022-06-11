9d85478fe5539cfee682f4c4edfd571101febe4d
Sidney Fernando <88115743+nando3d3@users.noreply.github.com>
2022-06-11 14:23:22 -0300

    Incrementando dados e tirando MPU

--- gps_sender/gps_sender.ino
+++ gps_sender/gps_sender.ino
@@ -25,13 +25,6 @@
 #include <mySD.h>
 #include <TinyGPS++.h>
 #include "heltec.h"
-//MPU
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
-
-// definir um nome para manipilar melhor 
-Adafruit_MPU6050 mpu;
 
 #define MICROSD_PIN_CHIP_SELECT   4
 #define MICROSD_PIN_MOSI          13
@@ -43,121 +36,40 @@ Adafruit_MPU6050 mpu;
 #define LOG_FILE_SUFFIX "csv" // Sufixo do arquivo
 char logFileName[13]; // Char string para o nome do arquivo
 // Data to be logged:
-#define LOG_COLUMN_COUNT 11
+#define LOG_COLUMN_COUNT 7
 char * log_col_names[LOG_COLUMN_COUNT] = {
- "hora", "longitude", "latitude", "altitude", "acelX", "acelY", "acelZ", "gyroX", "gyroY", "gyroZ", "temp"
+ "hora", "latitude", "longitude", "altitude(m)", "velocidade(km/h)", "curso(°)", "satelites"
 };
 
 
-TinyGPSPlus tinyGPS; // tinyGPSPlus object to be used throughout
+TinyGPSPlus tinyGPS; // objeto tinyGPSPlus para ser usado em tudo
 #define GPS_BAUD 9600
-#define BAND    915E6 //433E6  //you can set band here directly,e.g. 868E6,915E6
+#define BAND    920E6 //433E6  //Escolha a frequência
 String outh; //String para armazenar a hora
 String outlg; //String para armazenar a longitude
 String outlt; //String para armazenar a latitude
 String outalt; //String para armazenar a altitude
-
+String velo; //String para armazenar a velocidade
+String sat;
+String curso;
+int testSD = 1; //String para testar SD
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1,2,17); //RX (GPS) -> 17 (ESP); TX (GPS) -> 2;
   //Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, false /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
   Heltec.begin(false, true, false, true, BAND);
-  /*Heltec.display->init();
-  Heltec.display->flipScreenVertically();  
-  Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 10, "Iniciando...");
-  Heltec.display->display();
-  delay(1000);*/
 
   //SD
   Serial.println("Configurando SD Card");
   if (!SD.begin(MICROSD_PIN_CHIP_SELECT, MICROSD_PIN_MOSI, MICROSD_PIN_MISO, MICROSD_PIN_SCK)) {
     Serial.println("Falha na inicialização");
+    testSD = 0;
   }
 
   updateFileName(); // Toda vez que inicia, cria um novo arquivo gpslog(x+1).csv
   printHeader(); // Coloca o cabeçalho no arquivo novo
-  // --- Fim da inicialização do SD ---
-
-  //MPU
-  Serial.println("Adafruit MPU6050 test!");
-
-  // inicializar MPU 
-  if (!mpu.begin(//Testar outras portas além da 21 e 22)) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
-  
-  // faixa de medição do acelerômetro
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // faixa de medição do giroscópio
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  // faixa de medição do giroscópio 
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  //Fim da configuração do MPU
 }
 
 void loop()
@@ -167,28 +79,31 @@ void loop()
       {
       logGPSData();
 
+      // Imprime hora, latitude, longitude e altitude no seral
+      Serial.println(outh);
+      Serial.println(outlt);
+      Serial.println(outlg);
+      Serial.println(outalt);
+      Serial.println(velo);
+      Serial.println(curso);
+      Serial.println(sat);
+      Serial.print('\n');
+      if (testSD == 0) {
+        Serial.println("Falha em salvar no SD!");
+      }
       //Transmite os dados
       LoRa.beginPacket();
       LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
       LoRa.print(outh + "\n");
       LoRa.print(outlt + "\n");
       LoRa.print(outlg + "\n");
-      LoRa.print(outalt);
-      LoRa.print(a.acceleration.x);
-      LoRa.print(a.acceleration.y);
-      LoRa.print(a.acceleration.z);
-      LoRa.print(g.gyro.x);
-      LoRa.print(g.gyro.y);
-      LoRa.print(g.gyro.z);
-      LoRa.print(temp.temperature);
+      LoRa.print(outalt + "\n");
+      LoRa.print(velo + "\n");
+      LoRa.print(curso + "\n");
+      LoRa.print(sat + "\n");
+      LoRa.print("SD: " + String(testSD));
       LoRa.endPacket();
-      /*Heltec.display->clear();
-      Heltec.display->drawString(5, 5, outh);
-      Heltec.display->drawString(5, 20, outlt);
-      Heltec.display->drawString(5, 35, outlg);
-      Heltec.display->drawString(5, 50, outalt);
-      Heltec.display->display();*/
-      
+
       }
   if (millis() > 5000 && tinyGPS.charsProcessed() < 10)
   {
@@ -199,7 +114,6 @@ void loop()
 
 byte logGPSData()
 {
-  File logFile = SD.open(logFileName, FILE_WRITE); // Abre o arquivo
 
   //Configuração da hora
   String hrs = "";
@@ -238,54 +152,32 @@ byte logGPSData()
   strAlt = strAlt + "ALT: " + buff3; 
   outalt = strAlt;
 
-  //MPU
-  /* Modo que vai ser visualizado o dados */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  
-  //Configuração do acelerômetro
-  
+  velo = "VEL: " + String(tinyGPS.speed.mph()*1.609, 2) + " km/h";
+  sat = "SAT: " + String(tinyGPS.satellites.value());
+  curso = "Curso: " + String(tinyGPS.course.deg(), 1) + "°";
+
+  File logFile = SD.open(logFileName, FILE_WRITE); // Abre o arquivo
   
   if (logFile)
   { 
     logFile.print(hrs);
-    logFile.print('\t');
-    logFile.print(tinyGPS.location.lng(), 6);
-    logFile.print('\t');
-    Serial.println(outh);
+    logFile.print(',');
     logFile.print(tinyGPS.location.lat(), 6);
-    logFile.print('\t');
-    Serial.println(outlt);
+    logFile.print(',');
+    logFile.print(tinyGPS.location.lng(), 6);
+    logFile.print(',');
     logFile.print(tinyGPS.altitude.meters(), 1);
-    logFile.print('\t');
-    logFile.print(a.acceleration.x);
-    logFile.print('\t');
-    logFile.print(a.acceleration.y);
-    logFile.print('\t');
-    logFile.print(a.acceleration.z);
-    logFile.print('\t');
-    logFile.print(g.gyro.x);
-    logFile.print('\t');
-    logFile.print(g.gyro.y);
-    logFile.print('\t');
-    logFile.print(g.gyro.z);
-    logFile.print('\t');
-    logFile.print(temp.temperature);
-    Serial.println(outlg);
-    Serial.println(outalt);
-    Serial.println(a.acceleration.x);
-    Serial.println(a.acceleration.y);
-    Serial.println(a.acceleration.z);
-    Serial.println(g.gyro.x);
-    Serial.println(g.gyro.y);
-    Serial.println(g.gyro.z);
-    Serial.println(temp.temperature);
-    Serial.print('\n');
+    logFile.print(',');
+    logFile.print(tinyGPS.speed.mph()*1.609, 2);
+    logFile.print(',');
+    logFile.print(tinyGPS.course.deg(), 1);
+    logFile.print(',');
+    logFile.print(tinyGPS.satellites.value());
     logFile.println();
     logFile.close();
  
     return 1; // Retorna em caso de true
-  }
+  } 
  
   return 0; // Se falhar em abrir o arquivo, retorna false
 }
@@ -293,20 +185,20 @@ byte logGPSData()
 // printHeader() - coloca o cabeçalho no arquivo
 void printHeader()
 {
-  File logFile = SD.open(logFileName, FILE_WRITE); // Open the log file
+  File logFile = SD.open(logFileName, FILE_WRITE); // Abre o arquivo log
  
-  if (logFile) // If the log file opened, print our column names to the file
+  if (logFile) // Se o arquivo abrir, as colunas são colocadas
   {
     int i = 0;
     for (; i < LOG_COLUMN_COUNT; i++)
     {
       logFile.print(log_col_names[i]);
-      if (i < LOG_COLUMN_COUNT - 1) // If it's anything but the last column
-        logFile.print('\t'); // print a comma
+      if (i < LOG_COLUMN_COUNT - 1) // Se não há nada além da última coluna a ser colocada
+        logFile.print(','); // coloca uma vírgula
       else // If it's the last column
-        logFile.println(); // print a new line
+        logFile.println(); // coloca uma nova linha
     }
-    logFile.close(); // close the file
+    logFile.close(); // fecha o arquivo
   }
 }
  
